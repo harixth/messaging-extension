d92350c3997856a0eb20b858d05293df156442cd
Mohammad Harith <15016508+harixth@users.noreply.github.com>
2022-10-06 09:23:58 +0800

    Initial commit

---
+++ src/main/java/com/example/messagingextension/MessagingExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagingextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingExtensionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/messagingextension/MessagingExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingExtensionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
